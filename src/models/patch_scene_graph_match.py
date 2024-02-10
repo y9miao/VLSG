6eb78dad63746878fa9e30c9adeea788ce7a9701
y9miao <yamiao@student.ethz.ch>
2024-02-10 23:26:18 +0100

    refactor SGAligner for ablation study

---
+++ src/models/patch_scene_graph_match.py
@@ -0,0 +1,301 @@
+import os, sys
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(parent_dir)
+src_dir = os.path.dirname(parent_dir)
+sys.path.append(src_dir)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# 2D image feature extractor
+from GCVit.models import gc_vit
+# 3D scene graph feature extractor
+from src.models.sg_encoder_compact import SceneGraphEncoder, Mlps
+
+
+def _to_channel_last(x):
+    """
+    Args:
+        x: (B, C, H, W)
+
+    Returns:
+        x: (B, H, W, C)
+    """
+    return x.permute(0, 2, 3, 1)
+
+
+def _to_channel_first(x):
+    """
+    Args:
+        x: (B, H, W, C)
+
+    Returns:
+        x: (B, C, H, W)
+    """
+    return x.permute(0, 3, 1, 2)
+
+class PatchSceneGraphAligner(nn.Module):
+    def __init__(self, 
+                 backbone,
+                 num_reduce,
+                 backbone_dim,
+                 patch_hidden_dims,
+                 patch_encoder_dim,
+                 scene_graph_modules,
+                 scene_graph_in_dims,
+                 scene_graph_encode_depth,
+                 scene_graph_emb_dims,
+                 gat_hidden_units,
+                 gat_heads,
+                 scene_graph_node_dim,
+                 node_out_dim,
+                 drop,
+                 use_temporal):
+        super().__init__()
+        
+        # backbone 
+        self.backbone = backbone
+        if num_reduce > 0:
+            reduce_list = [gc_vit.ReduceSize(dim=backbone_dim, keep_dim=True)
+                                for i in range(num_reduce)]
+            self.reduce_layers = nn.Sequential(*reduce_list)
+        else:
+            self.reduce_layers = EncodeChannelSize(backbone_dim, backbone_dim, norm_layer=nn.LayerNorm)
+        
+        # patch feature encoder
+        self.patch_encoder = Mlps(backbone_dim, hidden_features = patch_hidden_dims, 
+                                 out_features= patch_encoder_dim, drop = drop)
+        
+        # 3D scene graph encoder
+        scene_graph_in_dims_dict = {scene_graph_modules[i]: scene_graph_in_dims[i] \
+            for i in range(len(scene_graph_modules))}
+        scene_graph_emb_dims_dict = {scene_graph_modules[i]: scene_graph_emb_dims[i] \
+            for i in range(len(scene_graph_modules))}
+        scene_graph_encode_depth_dict = {scene_graph_modules[i]: scene_graph_encode_depth[i] \
+            for i in range(len(scene_graph_modules))}
+        self.sg_encoder = SceneGraphEncoder(scene_graph_modules, 
+                                            in_dims = scene_graph_in_dims_dict,
+                                            encode_depth = scene_graph_encode_depth_dict,
+                                            encode_dims = scene_graph_emb_dims_dict,
+                                            gat_hidden_units=gat_hidden_units,
+                                            gat_heads = gat_heads,
+                                            dropout = drop,
+                                            use_transformer_aggregator=False)
+        self.sg_node_dim_match = nn.Linear(scene_graph_node_dim, node_out_dim)
+        
+        # use temporal information
+        self.use_temporal = use_temporal
+        
+    def forward(self, data_dict):
+        # get data
+        images = data_dict['images'] # (B, H, W, C)
+        channel_last = True
+        
+        # patch encoding 
+        images = _to_channel_first(images)
+        channel_last = False
+        features = self.backbone(images)[-1] # (B, C', H/32, W/32); input channel first,output channel first 
+        features = _to_channel_last(features)
+        channel_last = True
+        patch_features = self.reduce_layers(features) # (B, H/64, W/64, C'); input channel last,output channel last 
+        patch_features = self.patch_encoder(patch_features) # (B, P_H, P_W, C*); input channel last,output channel last 
+        patch_features = patch_features.flatten(1, 2) # (B, P_H*P_W, C*)
+        
+        # sg encoding
+        obj_3D_embeddings = self.forward_scene_graph(data_dict)  # (O, C*)
+        obj_3D_embeddings = self.sg_node_dim_match(obj_3D_embeddings) # (O, C*)
+        obj_3D_embeddings_norm = F.normalize(obj_3D_embeddings, dim=-1)
+        ## as long as batch_size^2 < batch_size*num_candidates^2, it is faster to calculate similarity between objs and patches
+        obj_3D_embeddings_sim = torch.mm(obj_3D_embeddings_norm, obj_3D_embeddings_norm.permute(1, 0)) # (O, O)
+         
+        # calculate similarity between patches and objs
+        batch_size = data_dict['batch_size']
+        
+        patch_obj_sim_list = []
+        patch_patch_sim_list = []
+        obj_obj_sim_list = []
+        patch_obj_sim_temp_list = []
+        patch_patch_sim_temp_list = []
+        obj_obj_sim_temp_list = []
+        
+        for batch_i in range(batch_size):
+            # calculate similarity between patches and objs
+            # patch features per batch
+            patch_features_pb = patch_features[batch_i] # (P_H*P_W, C*)
+            patch_features_pb_norm = F.normalize(patch_features_pb, dim=-1)
+            assoc_data_dict = data_dict['assoc_data_dict'][batch_i]
+            patch_obj_sim, patch_patch_sim, obj_obj_sim = self.calculate_similarity(
+                obj_3D_embeddings_norm, patch_features_pb_norm, assoc_data_dict, obj_3D_embeddings_sim)
+            patch_obj_sim_list.append(patch_obj_sim)
+            patch_patch_sim_list.append(patch_patch_sim)
+            obj_obj_sim_list.append(obj_obj_sim)
+            # temporal information
+            if self.use_temporal:
+                assoc_data_dict = data_dict['assoc_data_dict_temp'][batch_i]
+                patch_obj_sim_temp, patch_patch_sim_temp, obj_obj_sim_temp = self.calculate_similarity(
+                    obj_3D_embeddings_norm, patch_features_pb_norm, assoc_data_dict, obj_3D_embeddings_sim)
+                patch_obj_sim_temp_list.append(patch_obj_sim_temp)
+                patch_patch_sim_temp_list.append(patch_patch_sim_temp)
+                obj_obj_sim_temp_list.append(obj_obj_sim_temp)
+
+        embs = {}
+        embs['patch_raw_features'] = features # (B, H/32, W/32, C');
+        embs['patch_features'] = patch_features # (B, P_H*P_W, C*)
+        embs['obj_features'] = obj_3D_embeddings # [O, C*]
+        embs['patch_obj_sim'] = patch_obj_sim_list # B - [P_H*P_W, N]
+        embs['patch_patch_sim'] = patch_patch_sim_list # B - [P_H*P_W, P_H*P_W]
+        embs['obj_obj_sim'] = obj_obj_sim_list # B - [N, N]
+        embs['patch_obj_sim_temp'] = patch_obj_sim_temp_list # B - [P_H*P_W, N]
+        embs['patch_patch_sim_temp'] = patch_patch_sim_temp_list # B - [P_H*P_W, P_H*P_W]
+        embs['obj_obj_sim_temp'] = obj_obj_sim_temp_list
+        return embs
+    
+    def calculate_similarity(self, obj_3D_embeddings_norm, patch_features_norm, 
+                             assoc_data_dict, obj_3D_embeddings_sim):
+        scan_objs_idx = assoc_data_dict['scans_sg_obj_idxs']
+        scans_obj_embeddings_norm = obj_3D_embeddings_norm[scan_objs_idx, :] # (N, C*)
+        patch_obj_sim = torch.mm(patch_features_norm, scans_obj_embeddings_norm.permute(1, 0)) # (P_H*P_W, N)
+        patch_patch_sim = torch.mm(patch_features_norm, patch_features_norm.permute(1, 0)) # (P_H*P_W, P_H*P_W)
+        obj_obj_sim = obj_3D_embeddings_sim[scan_objs_idx, :][:, scan_objs_idx] # (N, N)
+        return patch_obj_sim, patch_patch_sim, obj_obj_sim
+    
+    def forward_with_patch_features(self, data_dict):
+        # get data
+        patch_features = data_dict['patch_features'] # (B, P_H*2, P_W*2, C*)
+        
+        # encoding patch features
+        patch_features = self.reduce_layers(patch_features) 
+        patch_features = self.patch_encoder(patch_features) # (B, P_H, P_W, C*)
+        patch_features = patch_features.flatten(1, 2) # (B, P_H*P_W, C*)
+        
+        # sg encoding
+        obj_3D_embeddings = self.forward_scene_graph(data_dict)  # (O, C*)
+        obj_3D_embeddings = self.sg_node_dim_match(obj_3D_embeddings) # (O, C*)
+        obj_3D_embeddings_norm = F.normalize(obj_3D_embeddings, dim=-1)
+        ## as long as batch_size^2 < batch_size*num_candidates^2, it is faster to calculate similarity between objs and patches
+        obj_3D_embeddings_sim = torch.mm(obj_3D_embeddings_norm, obj_3D_embeddings_norm.permute(1, 0)) # (O, O)
+         
+        # calculate similarity between patches and objs
+        batch_size = data_dict['batch_size']
+        
+        patch_obj_sim_list = []
+        patch_patch_sim_list = []
+        obj_obj_sim_list = []
+        patch_obj_sim_temp_list = []
+        patch_patch_sim_temp_list = []
+        obj_obj_sim_temp_list = []
+        
+        for batch_i in range(batch_size):
+            # calculate similarity between patches and objs
+            # patch features per batch
+            patch_features_pb = patch_features[batch_i] # (P_H*P_W, C*)
+            patch_features_pb_norm = F.normalize(patch_features_pb, dim=-1)
+            assoc_data_dict = data_dict['assoc_data_dict'][batch_i]
+            patch_obj_sim, patch_patch_sim, obj_obj_sim = self.calculate_similarity(
+                obj_3D_embeddings_norm, patch_features_pb_norm, assoc_data_dict, obj_3D_embeddings_sim)
+            patch_obj_sim_list.append(patch_obj_sim)
+            patch_patch_sim_list.append(patch_patch_sim)
+            obj_obj_sim_list.append(obj_obj_sim)
+            # temporal information
+            if self.use_temporal:
+                assoc_data_dict = data_dict['assoc_data_dict_temp'][batch_i]
+                patch_obj_sim_temp, patch_patch_sim_temp, obj_obj_sim_temp = self.calculate_similarity(
+                    obj_3D_embeddings_norm, patch_features_pb_norm, assoc_data_dict, obj_3D_embeddings_sim)
+                patch_obj_sim_temp_list.append(patch_obj_sim_temp)
+                patch_patch_sim_temp_list.append(patch_patch_sim_temp)
+                obj_obj_sim_temp_list.append(obj_obj_sim_temp)
+
+        embs = {}
+        embs['patch_features'] = patch_features # (B, P_H*P_W, C*)
+        embs['obj_features'] = obj_3D_embeddings # [O, C*]
+        embs['patch_obj_sim'] = patch_obj_sim_list # B - [P_H*P_W, N]
+        embs['patch_patch_sim'] = patch_patch_sim_list # B - [P_H*P_W, P_H*P_W]
+        embs['obj_obj_sim'] = obj_obj_sim_list # B - [N, N]
+        embs['patch_obj_sim_temp'] = patch_obj_sim_temp_list # B - [P_H*P_W, N]
+        embs['patch_patch_sim_temp'] = patch_patch_sim_temp_list # B - [P_H*P_W, P_H*P_W]
+        embs['obj_obj_sim_temp'] = obj_obj_sim_temp_list
+        return embs
+    
+    def forward2DImage(self, data_dict):
+        # get data
+        images = data_dict['images']
+        
+        # patch encoding 
+        images = _to_channel_first(images)
+        channel_last = False
+        features = self.backbone(images)[-1] # (B, C', H/32, W/32); input channel first,output channel first 
+        features = _to_channel_last(features)
+        channel_last = True
+        patch_features = self.reduce_layers(features) # (B, H/64, W/64, C'); input channel last,output channel last 
+        patch_features = self.patch_encoder(patch_features) # (B, P_H, P_W, C*); input channel last,output channel last 
+        patch_features = patch_features.flatten(1, 2) # (B, P_H*P_W, C*)
+        
+        return patch_features
+    
+    def forward_scene_graph(self, data_dict):
+        scene_graph_dict = data_dict['scene_graphs']
+        object_embeddings = self.sg_encoder(scene_graph_dict)
+        return object_embeddings
+
+class SE(nn.Module):
+    """
+    Squeeze and excitation block
+    """
+
+    def __init__(self,
+                 inp,
+                 oup,
+                 expansion=0.25):
+        """
+        Args:
+            inp: input features dimension.
+            oup: output features dimension.
+            expansion: expansion ratio.
+        """
+
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(oup, int(inp * expansion), bias=False),
+            nn.GELU(),
+            nn.Linear(int(inp * expansion), oup, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+class EncodeChannelSize(nn.Module):
+    def __init__(self,
+                 dim,
+                 dim_out,
+                 norm_layer=nn.LayerNorm):
+        """
+        Args:
+            dim: feature size dimension.
+            norm_layer: normalization layer.
+            keep_dim: bool argument for maintaining the resolution.
+        """
+
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(dim, dim, 3, 1, 1,
+                      groups=dim, bias=False),
+            nn.GELU(),
+            SE(dim, dim),
+            nn.Conv2d(dim, dim_out, 1, 1, 0, bias=False),
+        )
+        self.norm2 = norm_layer(dim_out)
+        self.norm1 = norm_layer(dim)
+
+    def forward(self, x):
+        x = x.contiguous()
+        x = self.norm1(x)
+        x = _to_channel_first(x)
+        x = x + self.conv(x)
+        x = _to_channel_last(x)
+        x = self.norm2(x)
+        return x
---
+++ src/models/sg_encoder_compact.py
@@ -0,0 +1,218 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+"""
+Mainly copy-paste from https://github.com/facebookresearch/detr/blob/master/models/transformer.py
+"""
+"""
+DETR Transformer class.
+
+Copy-paste from torch.nn.Transformer with modifications:
+    * positional encodings are passed in MHattention
+    * extra LN at the end of encoder is removed
+    * decoder returns a stack of activations from all decoding layers
+"""
+import copy
+from typing import Optional, List
+
+import torch
+import torch.nn.functional as F
+from torch import nn, Tensor
+
+# sgaliner modules 
+from models.sgaligner.src.aligner.networks.pointnet import PointNetfeat
+from torch_geometric.nn import GATConv, GCNConv
+
+class PatchAggregator(nn.Module):
+
+    def __init__(self, d_model, nhead, num_layers, dropout):
+        super().__init__()
+        # transformer encoders
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
+        # Initialize the [CLS] token
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, d_model))
+
+    def forward(self, x):
+        # x: (batch_size, num_patches, d_model)
+        cls_tokens = self.cls_token.expand(x.size(0), -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        output = self.transformer_encoder(x)
+        cls_token_output = output[:, 0, :]
+        return cls_token_output
+    
+class Mlps(nn.Module):
+    def __init__(self, in_features, hidden_features=[], out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        self.direct_output = False
+
+        layers_list = []
+        if(len(hidden_features) <= 0):
+            assert out_features == in_features
+            self.direct_output  = True
+            # layers_list.append(nn.Linear(in_features, out_features))
+            # layers_list.append(nn.Dropout(drop))
+            
+        else:
+            hidden_features = [in_features] + hidden_features
+            for i in range(len(hidden_features)-1):
+                layers_list.append(nn.Linear(hidden_features[i], hidden_features[i+1]))
+                layers_list.append(act_layer())
+                layers_list.append(nn.Dropout(drop))
+            layers_list.append(nn.Linear(hidden_features[-1], out_features))
+            layers_list.append(nn.Dropout(drop))
+            
+        self.mlp_layers =  nn.Sequential(*layers_list)
+
+    def forward(self, x):
+        if self.direct_output:
+            return x
+        else:
+            return self.mlp_layers(x)
+    
+class MultiGAT(nn.Module):
+    def __init__(self, n_units, n_gat_heads=[2, 2], dropout=0.0, use_edge_feat = False):
+        super(MultiGAT, self).__init__()
+        
+        self.use_edge_feat = use_edge_feat
+        self.num_layers = len(n_units)
+        self.dropout = dropout
+        
+        layer_stack = []
+        # in_channels, out_channels, gat_heads
+        for i in range(self.num_layers-1):
+            in_channels = n_units[i] * n_gat_heads[i-1] if i else n_units[i]
+            layer_stack.append(GATConv(in_channels=in_channels, out_channels=n_units[i+1], cached=False, gat_heads=n_gat_heads[i]))
+        self.layer_stack = nn.ModuleList(layer_stack)
+        
+    def forward(self, x, edges):
+        # TODO add support for edge features
+        for idx, gat_layer in enumerate(self.layer_stack):
+            x = F.dropout(x, self.dropout, training=self.training)
+            x = gat_layer(x, edges)
+            if idx+1 < self.num_layers:
+                x = F.elu(x)
+        return x
+    
+class SceneGraphEncoder(nn.Module):
+    def __init__(self, modules, 
+                 in_dims = {'point': 3, 'attr': 100, 'img_patch': 1536},
+                 encode_depth = {'point':[256, 256], 'img_patch': [512, 256]},
+                 encode_dims = {'point': 128, 'attr': 128, 'img_patch': 256},
+                 gat_hidden_units=[256, 256], gat_heads = [2, 2],
+                 dropout = 0.0,  use_transformer_aggregator=False):
+        super(SceneGraphEncoder, self).__init__()
+        self.modules = modules
+        self.in_dims = in_dims
+        self.encode_depth = encode_depth
+        self.encode_dims = encode_dims
+        self.gat_hidden_units = gat_hidden_units
+        self.gat_heads = gat_heads
+        
+        self.dropout = dropout
+        
+        # multi modal encoder models for nodes
+        self.context_num = 0
+        if 'point' in self.modules:
+            point_dim_in = self.in_dims['point']
+            geometry_dim = 256
+            point_encode_dim = self.encode_dims['point']
+            
+            self.geometry_encode = PointNetfeat(global_feat=True, batch_norm=True, point_size=point_dim_in, 
+                        input_transform=False, feature_transform=False, out_size=geometry_dim)
+            self.geometry_dim_encoder = Mlps(in_features=geometry_dim, hidden_features=encode_depth['point'], out_features=point_encode_dim)
+        
+        if 'attr' in self.modules:
+            attr_dim_in = self.in_dims['attr']
+            attr_encode_dim = self.encode_dims['attr']
+            self.attr_encoder = Mlps(in_features=attr_dim_in, hidden_features=encode_depth['attr'], out_features=attr_encode_dim)
+            
+        
+        if 'img_patch' in self.modules:
+            # whether to use transformer_encoder
+            self.use_transformer_aggregator = use_transformer_aggregator
+            img_feat_input_dim = self.in_dims['img_patch']
+            img_feat_encode_dim = self.encode_dims['img_patch']
+            self.multiview_encoder = PatchAggregator(d_model=img_feat_input_dim, nhead=2, num_layers=1, dropout=self.dropout)
+            self.img_patch_encoder = Mlps(in_features=img_feat_input_dim, 
+                        hidden_features=encode_depth['img_patch'], out_features=img_feat_encode_dim)
+        
+        ## TODO add rel encoder and edge feature embedding
+        
+        # graph attention network
+        # node_feat_dim = sum([self.encode_dims[module] for module in self.modules])
+        # gate_gat_hidden_units = [node_feat_dim] + gat_hidden_units
+        # self.gate = MultiGAT(gate_gat_hidden_units, n_gat_heads=gat_heads, dropout=dropout)
+            
+        
+    def forward(self, data_dict):
+        batch_size = data_dict['batch_size']
+        
+        object_points = data_dict['tot_obj_pts'].permute(0, 2, 1)
+        bow_vec_object_attr_feats = data_dict['tot_bow_vec_object_attr_feats'].float() 
+        bow_vec_object_edge_feats = data_dict['tot_bow_vec_object_edge_feats'].float()
+        rel_pose = data_dict['tot_rel_pose'].float()
+        
+        embs = {}
+        for module in self.modules:
+            # encode each module
+            # point geometry encoder
+            if module == 'point':
+                geo_features = self.geometry_encode(object_points)
+                geo_encode = self.geometry_dim_encoder(geo_features)
+                embs[module] = geo_encode
+                
+            elif module == 'attr':
+                attr_encode = self.attr_encoder(bow_vec_object_attr_feats)
+                embs[module] = attr_encode
+                
+            elif module == 'img_patch':
+                obs_img_patch_emb = None
+                start_object_idx = 0
+                for idx in range(batch_size):
+                    scan_id = data_dict['scene_ids'][idx][0]
+                    obj_count = data_dict['graph_per_obj_count'][idx][0]
+                    obj_ids = data_dict['obj_ids'][start_object_idx: start_object_idx + obj_count]
+                    img_patch_feat_scan = data_dict['obj_img_patches'][scan_id]
+                    for obj in obj_ids:
+                        img_patches = img_patch_feat_scan[obj]
+                        if self.use_transformer_aggregator:
+                            img_patches = img_patches.unsqueeze(0)
+                            img_patches_cls = self.multiview_encoder(img_patches)
+                            obs_img_patch_emb = torch.cat([img_patches_cls]) if obs_img_patch_emb is None else \
+                                        torch.cat([obs_img_patch_emb, img_patches_cls])
+                        else:
+                            obs_img_patch_emb = torch.cat([img_patches]) if obs_img_patch_emb is None else \
+                                        torch.cat([obs_img_patch_emb, img_patches])
+                    start_object_idx += obj_count
+
+                img_patch_encode = self.img_patch_encoder(obs_img_patch_emb)
+                embs[module] = img_patch_encode
+            else:
+                raise NotImplementedError
+        
+        # concatenate all embeddings
+        node_embs = None
+        for module in self.modules:
+            modular_embs = embs[module]
+            node_embs = torch.cat([node_embs, modular_embs], dim=1) \
+                if node_embs is not None else modular_embs
+                
+        return node_embs
+                
+        # # formulate edges
+        # cur_obj_num = 0
+        # start_edge_idx = 0
+        # edges = data_dict['edges']
+        # for idx in range(batch_size):
+        #     object_count = data_dict['graph_per_obj_count'][idx][0]
+        #     edges_count = data_dict['graph_per_edge_count'][idx][0]
+            
+        #     edges[start_edge_idx : start_edge_idx + edges_count] += cur_obj_num
+            
+        #     cur_obj_num += object_count
+        #     start_edge_idx += edges_count
+        # edges = torch.transpose(edges, 0, 1).to(torch.int32)
+        # # graph attention network
+        # embs = self.gate(node_embs, edges)
+        
+        # return embs
